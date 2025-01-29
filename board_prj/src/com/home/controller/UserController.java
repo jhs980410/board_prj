dcba52ebe49a39058d524eb256ee2a37dc2b047a
PC005\Win10 <skc05096@naver.com>
2025-01-29 20:03:45 +0900

    1. jhs   Controller 구현

--- board_prj/src/com/home/controller/UserController.java
+++ board_prj/src/com/home/controller/UserController.java
@@ -1,10 +1,27 @@
 package com.home.controller;
 
 public class UserController {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public String login(String userId, String userPw) {
+		try {
+			if (userId.isEmpty()) {
+				return "";
+			}else {
+					if ((userId.equals("양정") && userPw.equals("1234")) ) {
+					return userId;
+					}else {
+					return "회원이 아닙니다.";
+					}
+			}
+			
+			
+		} catch (NullPointerException e) {
+			return "";
+		}
+	
+		
+			
+			
+		
 	}
-
 }
